ef36a26cb1e5b3a3e16f31210827a095fa5d14fc
Abbas Abdallah <abbasab13@outlook.com>
2023-05-28 20:57:14 +0300

    updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL:'http://localhost:5000' })
-// const API = axios.create({ baseURL:'https://memories-backend-api.vercel.app' })
+// const API = axios.create({ baseURL:'http://localhost:5000' })
+const API = axios.create({ baseURL:'https://memories-backend-api.vercel.app' })
 
 API.interceptors.request.use(req => {
     if(localStorage.getItem('user')){
