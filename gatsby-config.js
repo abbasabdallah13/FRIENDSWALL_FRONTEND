d635942f5e454b607b19a7c60a8a9e32e0e9a22e
Abbas Abdallah <abbasab13@outlook.com>
2024-11-14 16:41:32 +0200

    added a favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,19 @@ module.exports = {
   flags: {
     DEV_SSR: false,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: `FriendsWall`,
+        short_name: `FriendsWall`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/memories.png`
+      }
+    }
+  ],
   
 }
