bb2e2e979764be1331a08405d0fbc6ed193036eb
Abbas Abdallah <abbasab13@outlook.com>
2024-11-15 13:44:52 +0200

    updated site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,24 +3,27 @@
  */
 module.exports = {
   siteMetadata: {
+    title: 'FriendsWall',
+    description: 'A social media platform where users can create accounts, share memories, and interact with each other.',
+    image: 'src/assets/memories.png',
     siteUrl: 'http://localhost:8000',
   },
   flags: {
     DEV_SSR: false,
   },
   plugins: [
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: `FriendsWall`,
-        short_name: `FriendsWall`,
-        start_url: `/`,
-        background_color: `#f7f0eb`,
-        theme_color: `#a2466c`,
-        display: `standalone`,
-        icon: `src/assets/memories.png`
-      }
-    }
+    // {
+    //   resolve: 'gatsby-plugin-manifest',
+    //   options: {
+    //     name: `FriendsWall`,
+    //     short_name: `FriendsWall`,
+    //     start_url: `/`,
+    //     background_color: `#f7f0eb`,
+    //     theme_color: `#a2466c`,
+    //     display: `standalone`,
+    //     icon: `src/assets/memories.png`
+    //   }
+    // }
   ],
   
 }
